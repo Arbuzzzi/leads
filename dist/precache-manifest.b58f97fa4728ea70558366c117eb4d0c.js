3a96b9acde061853d35208005c559e06a758e5d9
Arbuzzzi <safonov.mir@gmail.com>
2019-10-29 14:28:11 +0300

    gitignore

---
+++ dist/precache-manifest.b58f97fa4728ea70558366c117eb4d0c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "11c5a0acb5fd7f4da9bc7389c8705acc",
+    "url": "fonts/icomoon.woff"
+  },
+  {
+    "revision": "3bb6652f36b04a7d52475218052d0650",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "02d4c18cc204760ec0af",
+    "url": "js/chunk-vendors.fc22fa96.js"
+  },
+  {
+    "revision": "2e33ea46d1bf3f2453d989eaa9be72d1",
+    "url": "index.html"
+  },
+  {
+    "revision": "04f1f05eda89d84c1710",
+    "url": "js/app.6ac171ea.js"
+  },
+  {
+    "revision": "7b5c250c853e78018d334f3db4d92199",
+    "url": "fonts/icomoon.ttf"
+  },
+  {
+    "revision": "fc94ae4be7a4bef7bde62c1cbd7d416a",
+    "url": "fonts/icomoon.svg"
+  },
+  {
+    "revision": "368d5e65d3b83fd8434c3ac33f09a729",
+    "url": "fonts/icomoon.eot"
+  },
+  {
+    "revision": "c33f4cbdff3037a280293d74b62796e2",
+    "url": "fonts.css"
+  },
+  {
+    "revision": "02d4c18cc204760ec0af",
+    "url": "css/chunk-vendors.dd017920.css"
+  },
+  {
+    "revision": "04f1f05eda89d84c1710",
+    "url": "css/app.b6474991.css"
+  }
+];
